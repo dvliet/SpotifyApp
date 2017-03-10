8a9c4fffd26de1eccbe394c3b0911e6f760c6691
dvliet <dan.vliet@gmail.com>
2017-03-09 23:41:33 -0500

    Check in 4
    Adding in model files, adding repeatings divs

--- SpotifyApp/src/app/components/search/search.component.ts
+++ SpotifyApp/src/app/components/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {SpotifyService} from '../../services/spotify.service';
+import {Artist} from '../../../Artist';
 @Component({
     moduleId: module.id,
     selector: 'search',
@@ -7,14 +8,15 @@ import {SpotifyService} from '../../services/spotify.service';
 })
 export class SearchComponent {
     searchStr: string;
-
+    searchRes: Artist[];
     constructor(private _spotifyService:SpotifyService){
 
     }
 
     searchMusic(){
         this._spotifyService.searchMusic(this.searchStr).subscribe(res => {
-            console.log(res.artists.items);
+            console.log(res.artists.items);            
+            this.searchRes = res.artists.items;
         });
     }
 }
